53a121b02685f8143894e17b12ad16e8ed55e782
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2017-04-05 12:51:36 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,60 +4,112 @@ using namespace std;
 
 int main()
 {
-    double suhu,miuSuhu1,miuSuhu2,kelembapan,miuLembab1, miuLembab2;
-    string label1,label2; //label1 turun label2 naik
+    double suhu,miuSuhu1,miuSuhu2,kelembapan,miuLembab1,miuLembab2 = 0;
+    string label1suhu,label2suhu,label1lembab,label2lembab = " "; //label1 turun label2lembab naik
     cout<<"Masukkan Suhu: ";
     cin>>suhu;
-    if ((suhu <= 0) || ((suhu >= 3) && (suhu <= 12)) || ((suhu >= 15) && (suhu <= 24)) || ((suhu >= 27) && (suhu <= 36)) || (suhu >= 39)){
+    if (suhu <= 0){
         miuSuhu1 = 1;
+        label1suhu = "Dingin";
+        miuSuhu2 = 0;
+    }
+    else if ((suhu >= 3) && (suhu <= 12)){
+        miuSuhu1 = 1;
+        label1suhu = "Sejuk";
+        miuSuhu2 = 0;
+    }
+    else if ((suhu >= 15) && (suhu <= 24)){
+        miuSuhu1 = 1;
+        label1suhu = "Normal";
+        miuSuhu2 = 0;
+    }
+    else if ((suhu >= 27) && (suhu <= 36)){
+        miuSuhu1 = 1;
+        label1suhu = "Hangat";
+        miuSuhu2 = 0;
+    }
+    else if (suhu >= 39){
+        miuSuhu1 = 1;
+        label1suhu = "Panas";
+        miuSuhu2 = 0;
     }
     else if ((suhu > 0 && suhu < 3)){
         miuSuhu1 = -((suhu-3)/(3-0));
-        label1 = "Dingin";
+        label1suhu = "Dingin";
         miuSuhu2 = ((suhu-0)/3-0);
-        label2 = "Sejuk";
+        label2suhu = "Sejuk";
     }
     else if ((suhu > 12 && suhu < 15)){
         miuSuhu1 = -((suhu-15)/(15-12));
-        label1 = "Sejuk";
+        label1suhu = "Sejuk";
         miuSuhu2 = ((suhu-12)/(15-12));
-        label2 = "Normal";
+        label2suhu = "Normal";
     }
     else if ((suhu > 24 && suhu < 27)){
         miuSuhu1 = -((suhu-27)/(27-24));
-        label1 = "Normal";
+        label1suhu = "Normal";
         miuSuhu2 = ((suhu-24)/(27-24));
-        label2 = "Hangat";
+        label2suhu = "Hangat";
     }
     else if ((suhu > 36) && (suhu < 39)){
         miuSuhu1 = -((suhu-39)/(39-36));
-        label1 = "Hangat";
+        label1suhu = "Hangat";
         miuSuhu2 = ((suhu-36)/(39-36));
-        label2 = "Panas";
+        label2suhu = "Panas";
     }
 
-    cout<<label1<<"("<<miuSuhu1<<")"<<endl;
-    cout<<label2<<"("<<miuSuhu2<<")"<<endl;
-    cout<<"Masukan kelembapan : ";
+    //Menghitung kelembaban
+    cout<<"Masukan Kelembapan: ";
     cin>>kelembapan;
-    if((kelembapan<=10) || (kelembapan>=20 && kelembapan <=40) || (kelembapan >=50)){
-       miuLembab1 = 1;
-       miuLembab2 = 0;
+    if (kelembapan<=10){
+        miuLembab1 = 1;
+        label1lembab = "Kering";
+        miuLembab2 = 0;
     }
-    else if (kelembapan >10 && kelembapan <20){
+    else if ((kelembapan>=20) && (kelembapan <=40)){
+        miuLembab1 = 1;
+        label1lembab = "Lembab";
+        miuLembab2 = 0;
+    }
+    else if (kelembapan >=50){
+        miuLembab1 = 1;
+        label1lembab = "Basah";
+        miuLembab2 = 0;
+    }
+    else if ((kelembapan > 10) && (kelembapan <20)){
         miuLembab1 = -((kelembapan-20)/(20-10));
-        label1 = "Kering";
+        label1lembab = "Kering";
         miuLembab2 = (kelembapan-10)/(20-10);
-        label2 = "Lembab";
+        label2lembab = "Lembab";
     }
-    else if (kelembapan >20 && kelembapan <40){
+    else if ((kelembapan >20) && (kelembapan <40)){
         miuLembab1 = -((kelembapan-50)/(50-40));
-        label1 = "Lembap";
+        label1lembab = "Lembap";
         miuLembab2 = (kelembapan-40)/(50-40);
-        label2 = "Basah";
+        label2lembab = "Basah";
     }
-    cout<<label1<<"("<<miuLembab1<<")"<<endl;
-    cout<<label2<<"("<<miuLembab2<<")";
+
+
+    if ((miuLembab2 == 0) && (miuLembab2 == 0)){
+        cout<<label1suhu<<"("<<miuSuhu1<<")"<<endl;
+        cout<<label1lembab<<"("<<miuLembab1<<")"<<endl;
+    }
+    else if (miuSuhu2 == 0){
+        cout<<label1suhu<<"("<<miuSuhu1<<")"<<endl;
+        cout<<label1lembab<<"("<<miuLembab1<<")"<<endl;
+        cout<<label2lembab<<"("<<miuLembab2<<")";
+    }
+    else if (miuLembab2 == 0){
+        cout<<label1suhu<<"("<<miuSuhu1<<")"<<endl;
+        cout<<label2suhu<<"("<<miuSuhu2<<")"<<endl;
+        cout<<label1lembab<<"("<<miuLembab1<<")"<<endl;
+    }
+    else{
+        cout<<label1suhu<<"("<<miuSuhu1<<")"<<endl;
+        cout<<label2suhu<<"("<<miuSuhu2<<")"<<endl;
+        cout<<label1lembab<<"("<<miuLembab1<<")"<<endl;
+        cout<<label2lembab<<"("<<miuLembab2<<")";
+    }
 
     return 0;
 }
